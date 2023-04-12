a42f98f7361db41eae7577f889b0953a5e8ba453
Ksenia Suleimanova <57711937+Owl-coder@users.noreply.github.com>
2023-04-12 22:50:39 +0300

    Add files via upload

---
+++ initv6.js
@@ -0,0 +1,34 @@
+function init() {
+
+//
+//	let h3 = document.createElement('h3');
+//	h3.innerText = `Test ${version}`;
+//	document.body.prepend(h3);
+
+	let element = document.getElementById('canvas360');
+
+	let imagesArray = Array.from(new Array(40), (v, k) => { // generate array of urls
+		k = k+1;  // элементы начинаются с 1, а не с 0
+		let number = String(k).padStart(3, "0");
+		return `v6/dh-${number}.jpg`
+	});
+
+	var instance = new AnimateImages(element, {
+		images: imagesArray, /* required */
+		preload: "all",
+		preloadNumber: 0,
+		loop: true,
+		fps: 30,
+		poster: `v6/dh-001.jpg`,
+		draggable: true,
+		responsiveAspect: "height"
+	});
+
+	let start = document.getElementById('start');
+	let stop = document.getElementById('stop');
+
+	start.addEventListener('click', (e) => { instance.play(); });
+	stop.addEventListener('click', (e) => { instance.stop(); });
+
+}
+window.addEventListener('load', init)
